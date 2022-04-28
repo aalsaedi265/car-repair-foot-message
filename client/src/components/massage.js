5ca03e48b7438de2da5c50d675421fc7129a6064
Ahmed Alsaedi <aalsaedi265@gmail.com>
2022-04-28 13:33:39 -0500

    theme design and compoent creattion

---
+++ client/src/components/massage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+function massage(){
+
+    return (<></>)
+}
+
+export default massage
---
+++ client/src/components/payment.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+
+function payment () {
+
+    return (<></>)
+}
+
+export default payment
